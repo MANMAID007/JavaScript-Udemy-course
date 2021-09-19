d50c7ac4b607b4f9f3dbd2bec07d505cac34e940
MANMAID007 <youkti.smith@gmail.com>
2021-09-19 09:08:43 +0530

    codes added

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,1 +1,260 @@
 'use strict';
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  //   this.calcAge = function () {                  // never do this
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+const youkti = new Person('Youkti', 1993);
+console.log(youkti);
+console.log(youkti instanceof Person);
+
+Person.hey = function () {
+  console.log('Hey There!');
+};
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+console.log(youkti.calcAge());
+console.log(youkti.__proto__);
+console.log(youkti.__proto__.__proto__);
+console.dir(Person.prototype.constructor);
+
+const arr = [2, 6, 6, 8];
+console.log(arr.__proto__);
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(h1);
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+const Car = function (company, speed) {
+  this.company = company;
+  this.speed = speed;
+};
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  return this.speed;
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  return this.speed;
+};
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+console.log(`Car BMW: Acc: ${bmw.accelerate()}, brake: ${bmw.brake()}`);
+console.log(`Car BMW: Acc: ${bmw.accelerate()}, brake: ${bmw.brake()}`);
+console.log(
+  `Car Mercedes: Acc: ${mercedes.accelerate()}, brake: ${mercedes.brake()}`
+);
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+  static hey() {
+    console.log('Hey There!!!');
+  }
+}
+const youkti2 = new PersonCl('Youkti', 1993);
+youkti2.calcAge();
+
+const PersonProto = {
+  calcAge: function () {
+    console.log(2037 - this.birthYear);
+  },
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+const youktiProto = Object.create(PersonProto);
+youktiProto.init('Youkti', 1993);
+youktiProto.calcAge();
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+
+GOOD LUCK 😀
+*/
+// class CarCl {
+//   constructor(company, speed) {
+//     this.company = company;
+//     this.speed = speed;
+//   }
+
+//   get speedUS() {
+//     return this.speed / 1.6;
+//   }
+//   set speedUS(speed) {
+//     this.speed = speed * 1.6;
+//   }
+// }
+// inhetence
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study at ${this.course}`);
+};
+const youktiSt = new Student('Youkti', 1993, 'Statistics');
+console.log(youktiSt.introduce());
+console.log(youktiSt.calcAge());
+/* 
+1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
+3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
+4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism 😉
+
+DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+const EV = function (company, speed, charge = 80) {
+  Car.call(this, company, speed);
+  this.charge = charge;
+};
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.constructor = EV;
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge -= 1;
+  console.log(
+    `${this.company} going at ${this.speed}, with a charge of ${this.charge}`
+  );
+};
+const tesla = new EV('Tesla', 120, 23);
+tesla.chargeBattery(80);
+console.log(tesla.accelerate());
+console.log(tesla.accelerate());
+console.log(tesla.brake());
+
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    super(fullName, birthYear);
+    this.course = course;
+  }
+  introduce() {
+    console.log(`Hi, I am ${this.fullName}, doing course ${this.course}`);
+  }
+}
+const youktiStCl = new StudentCl('Youkti', 1993, 'Statistics');
+console.log(youktiStCl);
+console.log(youktiStCl.fullName);
+youktiStCl.calcAge();
+youktiStCl.introduce();
+class BankAccount {
+  locale = navigator.language;
+  #movements = [];
+  #pin;
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.#pin = pin;
+    console.log(`Thanks ${this.owner} for creating an account.`);
+  }
+  getMovementsSummary() {
+    const arr = this.#movements;
+    const total = arr.reduce((val, inc) => val + inc, 0);
+    console.log(`Your transactions are ${arr}. Total valuation ${total}`);
+  }
+  deposit(val) {
+    this.#movements.push(val);
+  }
+  withdraw(val) {
+    this.deposit(-val);
+  }
+  #approveLoan(val) {
+    return true;
+  }
+  requestLoan(val) {
+    if (this.#approveLoan) {
+      this.deposit(val);
+    }
+    console.log('Loan approved!!');
+  }
+}
+const youktiAcc = new BankAccount('Youkti', 'INR', 1993);
+youktiAcc.deposit(1200);
+youktiAcc.withdraw(400);
+youktiAcc.getMovementsSummary();
+console.log(youktiAcc);
+// console.log(youktiAcc.#movements);
+/* 
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+class CarCl {
+  constructor(company, speed) {
+    this.company = company;
+    this.speed = speed;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+  accelerate() {
+    this.speed += 10;
+    return this;
+  }
+  brake() {
+    this.speed -= 5;
+    return this;
+  }
+}
+class EVCl extends CarCl {
+  #charge;
+  constructor(company, speed, charge) {
+    super(company, speed);
+    this.#charge = charge;
+  }
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return chargeTo;
+  }
+  accelerate() {
+    this.speed += 20;
+    this.#charge -= 1;
+    return this;
+  }
+}
+const rivian = new EVCl('Rivian', 120, 23);
+console.log(rivian);
+console.log(rivian.accelerate().brake());
--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -3,4 +3,174 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 10000000
+            ).toFixed(2)} crores people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+          </div>
+        </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 ///////////////////////////////////////
+// const getCountryAndNeighbour = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+//   request.send();
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     // console.log(data);
+//     // console.log(data.name);
+//     renderCountry(data);
+//     const [neighbour] = data.borders;
+//     if (!neighbour) return;
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+//     request2.send();
+//     request2.addEventListener('load', function () {
+//       const data2 = JSON.parse(this.responseText);
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
+// getCountryAndNeighbour('turkey');
+// getCountryData('usa');
+// getCountryData('uk');
+// const request3 = fetch('https://restcountries.eu/rest/v2/name/bharat')
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function ([data]) {
+//     btn.addEventListener('click', function () {
+//       renderCountry(data);
+//     });
+//   })
+//   .catch(err => alert(err));
+/* 
+In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://geocode.xyz/api.
+The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+
+GOOD LUCK 😀
+*/
+const renderCountryFromCoords = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+          <div class="country__data">
+            <h3 class="country__name">${data.countryName}</h3>
+            <h4 class="country__region">${data.continent}</h4>
+            <p class="country__row"><span>👫</span>${(
+              60000000 / 10000000
+            ).toFixed(2)} crores people</p>
+            <p class="country__row"><span>🗣️</span>${
+              data.localityLanguageRequested
+            }</p>
+            <p class="country__row"><span>💰</span>KKK</p>
+          </div>
+        </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+};
+const whereAmI = function (lat, lng) {
+  const request =
+    fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en
+  `)
+      .then(request => request.json())
+      .then(data => {
+        console.log(`You are at ${data.countryName}`);
+        return data;
+      })
+      .then(data =>
+        btn.addEventListener('click', () => renderCountryFromCoords(data))
+      )
+      .catch(error => {
+        console.log(error);
+      })
+      .finally(() => {
+        countriesContainer.style.opacity = 1;
+      });
+};
+// whereAmI(52.508, 13.381);
+
+// const getPosition = function () {
+//   return new Promise((resolve, reject) => {
+//     // navigator.geolocation.getCurrentPosition(
+//     //   position => {
+//     //     resolve(position);
+//     //   },
+//     //   err => reject(err)
+//     // );
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// };
+// getPosition().then(pos =>
+//   console.log(pos.coords.latitude, pos.coords.longitude)
+// );
+/* 
+Build the image loading functionality that I just showed you on the screen.
+
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+
+PART 1
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+
+If this part is too tricky for you, just watch the first part of the solution.
+
+PART 2
+2. Comsume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+
+GOOD LUCK 😀
+*/
+let imageEl = document.createElement('img');
+const imgContainer = document.querySelector('.images');
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    imageEl.src = imgPath;
+
+    () => {
+      imgContainer.append(imageEl);
+      resolve(imageEl);
+    };
+    () => reject(new Error('Image not found'));
+  });
+};
+createImage('./img/img-1.jpg')
+  .then(img => {
+    console.log('image selected');
+    return img;
+  })
+  .catch(error => console.log(error))
+  .finally(() => (countriesContainer.style.opacity = 1));
